59a215e875e78d96a2db776767887425c03c19e2
Oskar <oskar.case@gmail.com>
2019-12-28 15:59:54 +0800

    Changing dom with state and setState

--- src/App.js
+++ src/App.js
@@ -4,12 +4,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Michael', age: 24},
+      {name: 'Jess', age: 18},
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log('Clicked')
+    this.setState( {
+      persons: [
+        {name: 'Maximus', age: 6000},
+        {name: 'Michael', age: 24},
+        {name: 'G.I.JOSE', age: 0},
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>This is really working!</p>
-        <Person name="Case" age="25">Hobbies: Racing</Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
