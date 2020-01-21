dd8eefd5dbe3438d5dd22bceff150d42216424e5
Oskar <oskar.case@gmail.com>
2020-01-21 22:04:17 +0800

    Removed lifecycle warnings and some other changes

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,10 +3,14 @@ import React, { Component }  from 'react';
 import Person from './Person/Person'
 
 class Persons extends Component {
-  static getDerivedStateFromProps(props, state) {
-    console.log('[Persons.js] getDerivedStateFromProps');
-    return state;
-  }
+  //static getDerivedStateFromProps(props, state) {
+    //console.log('[Persons.js] getDerivedStateFromProps');
+    //return state;
+  //}
+
+  //componentWillReceiveProps(props) {
+  //  console.log('[Persons.js] componentWillRecieveProps', props);
+  //}
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('[Persons.js] shouldComponentUpdate');
@@ -15,10 +19,12 @@ class Persons extends Component {
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
     console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return { message: 'Snapshot!' };
   }
 
-  componentDidUpdate() {
+  componentDidUpdate(prevProps, prevState, snapshot) {
     console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
   }
 
 
